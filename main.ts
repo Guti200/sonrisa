5dbf13eebc23458b3e2f9444d604e0b2a689d9df
Guti200 <130452905+Guti200@users.noreply.github.com>
2023-04-18 12:07:27 +0200

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -3,7 +3,16 @@ input.onButtonPressed(Button.A, function () {
     radio.sendString("smile")
 })
 radio.onReceivedString(function (receivedString) {
+    let sad = 0
     basic.showIcon(IconNames.Happy)
+    if (sad) {
+        basic.clearScreen()
+        basic.showIcon(IconNames.Sad)
+    }
+})
+input.onButtonPressed(Button.B, function () {
+    basic.clearScreen()
+    radio.sendString("sad")
 })
 radio.setGroup(69)
 basic.forever(function () {
